d3455ebf287e39216a35d77ff0f8f2a2bb01b4d9
Tony Baker <this.tonybaker@gmail.com>
2019-07-29 19:57:25 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190729195502) do
+ActiveRecord::Schema.define(version: 20190729195708) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "bio"
   end
 
   create_table "genres", force: :cascade do |t|
